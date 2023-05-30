9fc8671ee4f50f98801337077ee65274d92b566d
davidpitutiu <dpitutiu@icloud.com>
2023-05-30 18:53:04 +0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Pitutiu David on 30.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
